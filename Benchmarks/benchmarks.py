5dd07bc835d300518ff0e7a8ae3b5f4012a5fff5
F/Poutre <kevin@fpout.re>
2021-12-07 16:32:14 +0100

    added multi sample support for native python benchmark

--- Benchmarks/benchmarks.py
+++ Benchmarks/benchmarks.py
@@ -1,76 +1,87 @@
 import argparse
 import time
+import csv
+import random
 
 import numpy as np
+import statistics as stats
 from PIL import Image
 import tflite_runtime.interpreter as tflite
 
 
-def load_labels(filename):
-  with open(filename, 'r') as f:
-    return [line.strip() for line in f.readlines()]
-
-
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '-i',
-      '--image',
-      default='/tmp/grace_hopper.bmp',
-      help='image to be classified')
-  parser.add_argument(
-      '-m',
-      '--model_file',
-      default='/tmp/mobilenet_v1_1.0_224_quant.tflite',
-      help='.tflite model to be executed')
-  parser.add_argument(
-      '-l',
-      '--label_file',
-      default='/tmp/labels.txt',
-      help='name of file containing labels')
-  parser.add_argument(
-      '--input_mean',
-      default=127.5, 
-      type=float,
-      help='input_mean')
-  parser.add_argument(
-      '--input_std',
-      default=127.5, 
-      type=float,
-      help='input standard deviation')
-  parser.add_argument(
-      '--num_threads', 
-      default=4, 
-      type=int, 
-      help='number of threads')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d',
+        '--image_directory',
+        default='../Data/',
+        help='directory containing images to be classified, as well as DatabasePS4.csv')
+    parser.add_argument(
+        '-n',
+        '--image_number',
+        default=50,
+        type=int,
+        help='the number of images to benchmark on')
+    parser.add_argument(
+        '-m',
+        '--model_file',
+        default='/tmp/mobilenet_v1_1.0_224_quant.tflite',
+        help='.tflite model to be executed')
+    parser.add_argument(
+        '--input_mean',
+        default=127.5, 
+        type=float,
+        help='input_mean')
+    parser.add_argument(
+        '--input_std',
+        default=127.5, 
+        type=float,
+        help='input standard deviation')
+    parser.add_argument(
+        '--num_threads', 
+        default=4, 
+        type=int, 
+        help='number of threads')
+    args = parser.parse_args()
 
-  interpreter = tflite.Interpreter(
-      model_path=args.model_file, num_threads=args.num_threads)
-  interpreter.allocate_tensors()
+    database = []
+    with open(args.image_directory + '/DatabasePS4.csv', newline='') as csvfile:
+        csv_dict_reader = csv.DictReader(csvfile)
+        for row in csv_dict_reader:
+            database.append(row)
 
-  input_details = interpreter.get_input_details()
-  output_details = interpreter.get_output_details()
+    interpreter = tflite.Interpreter(
+        model_path=args.model_file, num_threads=args.num_threads)
+    interpreter.allocate_tensors()
 
-  # check the type of the input tensor
-  floating_model = input_details[0]['dtype'] == np.float32
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
-  # NxHxWxC, H:1, W:2
-  height = input_details[0]['shape'][1]
-  width = input_details[0]['shape'][2]
-  img = Image.open(args.image).resize((width, height))
+    # check the type of the input tensor
+    floating_model = input_details[0]['dtype'] == np.float32
 
-  # add N dim
-  input_data = np.expand_dims(img, axis=0)
+    # NxHxWxC, H:1, W:2
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
 
-  if floating_model:
-    input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+    time_list = []
 
-  interpreter.set_tensor(input_details[0]['index'], input_data)
+    for i in range(args.image_number):
+        file = database[random.randint(1, len(database)-1)]
+        img = Image.open("{}ImagesPS4/{}.jpg".format(args.image_directory, file["Images"])).resize((width, height))
 
-  start_time = time.time()
-  interpreter.invoke()
-  stop_time = time.time()
+        # add N dim
+        input_data = np.expand_dims(img, axis=0)
 
-  output_data = interpreter.get_tensor(output_details[0]['index'])
-  print("angle : {}, took {} µs".format(output_data[0][0], 1000*(stop_time-start_time)))
+        if floating_model:
+            input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+
+        start_time = time.time()
+        interpreter.invoke()
+        stop_time = time.time()
+
+        time_list.append(stop_time-start_time)
+    
+    print("Average: {} µs".format(1000*stats.mean(time_list)))
