4a4a896815ba7c03ec65b63b0e0636b04896a80a
F/Poutre <kevin@fpout.re>
2021-12-14 16:50:02 +0100

    added the signdetector thread to the orchestrator #8

--- Orchestrator/orchestrator.py
+++ Orchestrator/orchestrator.py
@@ -3,13 +3,17 @@ import sys
 
 import controller
 import lanefollower
+import signdetector
 
 picar = controller.PicarControl()
-laneFollower = lanefollower.LaneFollower(10, picar)
+laneFollower = lanefollower.LaneFollower(5, picar)
+signDetector = signdetector.SignDetector(5, picar)
 
 laneFollowerThread = Thread(target=lanefollower.continuousDetection, args=(laneFollower))
+signDetectorThread = Thread(target=signdetector.continuousDetection, args=(signDetector))
 
 laneFollowerThread.start()
+signDetectorThread.start()
 
 inputS = ""
 
@@ -17,4 +21,5 @@ while inputS != "exit":
     inputS = input("Enter \"exit\" to stop process.")
 
 laneFollowerThread.join()
+signDetectorThread.join()
 sys.exit()
