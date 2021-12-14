3dfe0882c6ef3943852ee27ad2ca9ee60d585bcc
F/Poutre <kevin@fpout.re>
2021-12-14 16:33:54 +0100

    corrected signdetector.py #5

--- Orchestrator/signdetector.py
+++ Orchestrator/signdetector.py
@@ -2,7 +2,7 @@ from time import sleep
 import numpy as np
 import tflite_runtime.interpreter as tflite
 import cv2
-from controller import PicarControl
+import skimage
 
 class SignDetector:
 
@@ -10,6 +10,9 @@ class SignDetector:
         self.freq = freq
 
         self.picar = controller
+        self.speedLimit = 50
+
+        self.cap = cv2.VideoCapture(0)
 
         self.interpreter = tflite.Interpreter("../RecognitionModel/model.tflite")
         self.interpreter.allocate_tensors()
@@ -18,41 +21,40 @@ class SignDetector:
         self.output_details = self.interpreter.get_output_details()
 
         # check the type of the input tensor
-        self.floating_model = self.input_details[0]['dtype'] == np.float32
+        self.input_shape = self.input_details[0]['shape']
 
-        # NxHxWxC, H:1, W:2
-        self.height = self.input_details[0]['shape'][1]
-        self.width = self.input_details[0]['shape'][2]
-
-        self.cap = cv2.VideoCapture(0)
-
+    """
+    Returns an int between 0 and 4 (included).
+    0:'Speed limit (30km/h)'
+    1:'Speed limit (50km/h)'
+    2:'No overtaking'
+    3:'Stop'
+    4:'No entry'
+    Only 0, 1 and 3 are needed here.
+    """
     def predict(self):
         ret, img = self.cap.read()
-        img = imgPreprocess(img)
-        np.reshape(img, (self.width, self.height))
+        img = skimage.transform.resize(img, (30, 30))
+        img = np.reshape(img, (-1,30,30,3)).astype('float32')
 
-        # add N dim
-        input_data = np.expand_dims(img, axis=0)
-
-        if self.floating_model:
-            input_data = (np.float32(input_data) - 127.5) / 127.5
-
-        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+        self.interpreter.set_tensor(self.input_details[0]['index'], img)
 
         self.interpreter.invoke()
 
-        output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-
-        return 35*output_data[0][0]
-
-def imgPreprocess(image):
-    height, _, _ = image.shape
-    image = image[int(height):,:,:]  # TODO Better cropping?
-    image = image / 255                # normalizing the pixel values 
-    return image
+        return np.argmax(self.interpreter.get_tensor(self.output_details[0]['index']))
 
 def continuousDetection(signDetector):
     while not False:
         sleep(1/signDetector.freq)
-        speed = signDetector.predict()
-        signDetector.picar.setSpeed(speed)
+        prediction = signDetector.predict()
+
+        if prediction == 0:
+            signDetector.picar.setSpeed(30)
+            signDetector.speedLimit = 30
+        elif prediction == 1:
+            signDetector.picar.setSpeed(50)
+            signDetector.speedLimit = 50
+        elif prediction == 3:
+            signDetector.picar.setSpeed(0)
+            sleep(3)
+            signDetector.picar.setSpeed(signDetector.speedLimit)
