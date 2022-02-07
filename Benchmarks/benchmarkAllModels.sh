49c38df75c10c4c65c670d13ab43f54f75d09f39
F/Poutre <kevin@fpout.re>
2022-02-07 13:37:57 +0000

    Modified benchmarkAllModels script to use python3

--- Benchmarks/benchmarkAllModels.sh
+++ Benchmarks/benchmarkAllModels.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Base Model"
-python benchmarks.py -m ../LaneFollowingModel/base/model.tflite -n 5000
+python3 benchmarks.py -m ../LaneFollowingModel/base/model.tflite -n 5000
 echo ""
 echo "Pruned Model"
-python benchmarks.py -m ../LaneFollowingModel/pruned/model.tflite -n 5000
+python3 benchmarks.py -m ../LaneFollowingModel/pruned/model.tflite -n 5000
 echo ""
 echo "Float16 Quantized Model"
-python benchmarks.py -m ../LaneFollowingModel/f16/model.tflite -n 5000
+python3 benchmarks.py -m ../LaneFollowingModel/f16/model.tflite -n 5000
 echo ""
 echo "Dynamic Range Quantized Model"
-python benchmarks.py -m ../LaneFollowingModel/dq/model.tflite -n 5000
+python3 benchmarks.py -m ../LaneFollowingModel/dq/model.tflite -n 5000
 echo ""
 echo "Integer (with float fallback) Quantized Model"
-python benchmarks.py -m ../LaneFollowingModel/intq/model.tflite -n 5000
+python3 benchmarks.py -m ../LaneFollowingModel/intq/model.tflite -n 5000
