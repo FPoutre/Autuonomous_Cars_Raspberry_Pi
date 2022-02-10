768f637284653cb7d5b9c56b523ba61d9e3b87a3
fpoutre <kevin.levy@etu.univ-cotedazur.fr>
2022-02-10 14:46:00 +0100

    imported final legacy model, and created a compatible benchmarks script

--- Pruning/preprocessorTest.py
+++ Pruning/preprocessorTest.py
@@ -3,13 +3,22 @@ import csv
 import random
 import numpy as np
 
-
-def my_imread(image_path):
+def imageReadLegacy(image_path):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
-def img_preprocess(image):
+def imgPreprocessLegacy(image):
+    height, *_ = image.shape
+    image = image[int(height/2):,:]  # remove top half of the image, as it is not relavant for lane following
+    return image
+
+def imageRead(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
+
+def imgPreprocess(image):
     height, *_ = image.shape
     image = image[int(height/2):,:]  # remove top half of the image, as it is not relavant for lane following
     _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
@@ -24,8 +33,13 @@ if __name__=="__main__":
             database.append(row)
     
     file = database[random.randint(1, len(database)-1)]
-    oFrame = my_imread("../Data/ImagesPS4/" + file["Images"] + ".jpg")
-    cv2.imwrite("../Data/train.png", oFrame)
 
-    pFrame = img_preprocess(oFrame)
-    cv2.imwrite("../Data/preprocessed_train.png", pFrame)
+    oLFrame = imageReadLegacy("../Data/ImagesPS4/" + file["Images"] + ".jpg")
+    cv2.imwrite("../Data/legacy.png", oLFrame)
+    pLFrame = imgPreprocessLegacy(oLFrame)
+    cv2.imwrite("../Data/preprocessed_legacy.png", pLFrame)
+
+    oNFrame = imageRead("../Data/ImagesPS4/" + file["Images"] + ".jpg")
+    cv2.imwrite("../Data/new.png", oNFrame)
+    pNFrame = imgPreprocess(oNFrame)
+    cv2.imwrite("../Data/preprocessed_new.png", pNFrame)
